b8f1b4ca776e50962cad0c9172a91b23bd15212c
yogendraft21 <yogendra311204@gmail.com>
2023-05-15 10:48:12 +0530

    Success

---
+++ leetcode/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    void inorder(TreeNode* root, vector<int>&res) {
+        if(!root) return;
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        vector<int> res;
+        inorder(root, res);
+        for(int i=1; i<res.size(); ++i) {
+            if(res[i-1]>=res[i]) return false;
+        }
+        return true;
+    }
+};
